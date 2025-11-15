dd080d6fc53ef16d28f0b3855373f04896f53a5a
Minot-Prod <maxime.grauliere@gmail.com>
2025-11-15 18:01:52 -0500

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"691905d72d10660018ba7cee",
+				"691905d72d10660018ba7cf1"
+			]
+		},
+		"691905d72d10660018ba7cee": {
+			"id": "691905d72d10660018ba7cee",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"691905d72d10660018ba7cf1": {
+			"id": "691905d72d10660018ba7cf1",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
